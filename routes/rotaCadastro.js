20eb541c1bb2457e6dac8b2a2d655567d4f7b1e9
Frank Gárate <frank.garaterios@gmail.com>
2022-07-27 23:25:43 -0300

    middleware multer no projeto + patials em cadastro/login

--- routes/rotaCadastro.js
+++ routes/rotaCadastro.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cadastroController = require('../controllers/cadastroController');
+const multer = require("multer")
 
 router.get("/cadastro", cadastroController.cadastro);
 
