f8720fb4ac68a74af20ce873e503d0b34a8b1f74
talitalduarte <talitalopesduarte@gmail.com>
2022-08-01 18:59:19 -0300

    atualizando as páginas de pagamento e sacola

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cadastroRouter = require('./routes/rotaCadastro');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', pathjoin(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
